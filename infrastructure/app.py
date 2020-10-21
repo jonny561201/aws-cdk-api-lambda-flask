e3b9882965b5aa432c82a21df549403d4b3c92e7
Jon Graf <jgraf@leantechniques.com>
2020-10-21 12:12:31 -0500

    added an additional environment workflow

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -5,6 +5,7 @@ from cdk.cdk_stack import InfrastructureStack
 
 app = core.App()
 
+InfrastructureStack(app, 'dev-aws-cdk-api-lambda-flask', env=core.Environment(account='', region='us-east-1'))
 InfrastructureStack(app, 'prod-aws-cdk-api-lambda-flask', env=core.Environment(account='', region='us-east-1'))
 
 app.synth()
