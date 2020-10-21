2c1614a8810c1cc3a43124e751dcc96fccb7dc1d
Jon Graf <jgraf@leantechniques.com>
2020-10-21 12:08:30 -0500

    added stack deployment to cdk in github actions

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -5,6 +5,6 @@ from cdk.cdk_stack import InfrastructureStack
 
 app = core.App()
 
-InfrastructureStack(app, 'aws-cdk-api-lambda-flask', env=core.Environment(account='', region='us-east-1'))
+InfrastructureStack(app, 'prod-aws-cdk-api-lambda-flask', env=core.Environment(account='', region='us-east-1'))
 
 app.synth()
